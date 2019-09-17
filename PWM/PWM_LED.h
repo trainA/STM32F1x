ab14b8c7bc378484a6a64b42e3540bf8d46b4cd2
training <253164597@qq.com>
2019-09-17 11:43:37 +0800

    增加使用PWM控制LED灯实现呼吸灯效果

---
+++ PWM/PWM_LED.h
@@ -0,0 +1,9 @@
+#ifndef PWM_LED_
+#define PWM_LED_
+
+#include "stm32f10x.h"
+#include "stm32f10x_tim.h"
+
+void TIM3_CH1_PWM_Init(u16 pre ,u16 psc);
+
+#endif 
--- User/main.c
+++ User/main.c
@@ -4,42 +4,64 @@
 #include "beep.h"
 #include "exit.h"
 #include "exti_tim.h"
+#include "PWM_LED.h"
 /*
 第一个参数设置 主频分频
 第二个参数设置 倍频
 */
-void RCC_HSE_Config(u32 div,u32 pllm) //自定义系统时间（可以修改时钟）
+void RCC_HSE_Config(u32 div, u32 pllm) //自定义系统时间（可以修改时钟）
 {
-	RCC_DeInit(); //将外设RCC寄存器重设为缺省值
-	RCC_HSEConfig(RCC_HSE_ON);//设置外部高速晶振（HSE）
-	if(RCC_WaitForHSEStartUp()==SUCCESS) //等待HSE起振
-	{
-		RCC_HCLKConfig(RCC_SYSCLK_Div1);//设置AHB时钟（HCLK）
-		RCC_PCLK1Config(RCC_HCLK_Div2);//设置低速AHB时钟（PCLK1）
-		RCC_PCLK2Config(RCC_HCLK_Div1);//设置高速AHB时钟（PCLK2）
-		RCC_PLLConfig(div,pllm);//设置PLL时钟源及倍频系数
-		RCC_PLLCmd(ENABLE); //使能或者失能PLL
-		while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY)==RESET);//检查指定的RCC标志位设置与否,PLL就绪
-		RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);//设置系统时钟（SYSCLK）
-		while(RCC_GetSYSCLKSource()!=0x08);//返回用作系统时钟的时钟源,0x08：PLL作为系统时钟
-	}
+    RCC_DeInit(); //将外设RCC寄存器重设为缺省值
+    RCC_HSEConfig(RCC_HSE_ON);//设置外部高速晶振（HSE）
+    if(RCC_WaitForHSEStartUp() == SUCCESS) //等待HSE起振
+    {
+        RCC_HCLKConfig(RCC_SYSCLK_Div1);//设置AHB时钟（HCLK）
+        RCC_PCLK1Config(RCC_HCLK_Div2);//设置低速AHB时钟（PCLK1）
+        RCC_PCLK2Config(RCC_HCLK_Div1);//设置高速AHB时钟（PCLK2）
+        RCC_PLLConfig(div, pllm); //设置PLL时钟源及倍频系数
+        RCC_PLLCmd(ENABLE); //使能或者失能PLL
+        while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET); //检查指定的RCC标志位设置与否,PLL就绪
+        RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);//设置系统时钟（SYSCLK）
+        while(RCC_GetSYSCLKSource() != 0x08); //返回用作系统时钟的时钟源,0x08：PLL作为系统时钟
+    }
 }
 
 int main(void)
 {
-//	uint32_t j;
-//	j = 0;
-	RCC_HSE_Config(RCC_PLLSource_HSE_Div1,RCC_PLLMul_9);
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-	exit_key_init();
-	TIM4_init(1000,36000-1);//500ms中断一次
-	SysTick_Init(72);
-	led_init();
-	beep_init();
-//	beep = 1;
-	beep_on();
-	while(1)
-		;
+
+    u16 i;
+    u8 fx;
+    //RCC_HSE_Config(RCC_PLLSource_HSE_Div1,RCC_PLLMul_9);
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+   //exit_key_init();
+   // TIM4_init(1000, 36000 - 1); //500ms中断一次
+    SysTick_Init(72);
+    led_init();
+    //	beep_init();
+    //	beep = 1;
+    TIM3_CH1_PWM_Init(500, 72 - 1);
+    while(1)
+    {
+
+        if(fx == 0)
+        {
+            i++;
+            if(i == 300)
+            {
+                fx = 1;
+            }
+        }
+        else
+        {
+            i--;
+            if(i == 0)
+            {
+                fx = 0;
+            }
+        }
+        TIM_SetCompare1(TIM3, i); //i值最大可以取499，因为ARR最大值是499.
+        delay_ms(10);
+    }
 //	{
 //		delay_ms(1000);
 //		led_on(j);
